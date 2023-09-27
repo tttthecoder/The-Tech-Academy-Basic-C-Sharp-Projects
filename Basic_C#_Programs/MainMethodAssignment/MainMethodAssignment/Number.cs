092052aa3babc1bcd299ff05cb9c1df7d64115a4
tttthecoder <trungtintran1905@gmail.com>
2023-09-26 20:45:39 -0600

    Added the folder for the Main Method Assignment.

---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Number
+    {
+        public int mathMethod(int num1)
+        {
+            return num1 + 1;
+        }
+        public int mathMethod(decimal num1)
+        {
+            return Convert.ToInt32(num1 + 1);
+        }
+        public int mathMethod(string num1)
+        {
+            return (Convert.ToInt32(num1) * 10);
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------method 1---------------------------");
+            Number numObject = new Number();
+            int num1 = numObject.mathMethod(9);
+            Console.WriteLine("The result of calling the mathMethod() method is: "+ num1);
+
+            Console.WriteLine("---------------------------method 2---------------------------");
+            Number numObject2 = new Number();
+            int num2 = numObject2.mathMethod(91.22m);
+            Console.WriteLine("The result of calling the decimal version of the mathMethod() method is: " + num2);
+
+            Console.WriteLine("---------------------------method 3---------------------------");
+            try
+            {
+                Number numObject3 = new Number();
+                int num3 = numObject3.mathMethod("91m");
+                Console.WriteLine("The result of calling the decimal version of the mathMethod() method is: " + num3);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("The operation done by the 3rd version of the method failed.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
