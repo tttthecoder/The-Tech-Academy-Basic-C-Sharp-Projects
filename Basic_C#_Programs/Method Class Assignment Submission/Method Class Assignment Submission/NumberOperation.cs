0cc9fefc433921f1088c69ccdc213030d61bf98a
tttthecoder <trungtintran1905@gmail.com>
2023-09-26 22:26:55 -0600

    Added the folder for the Method Class Assignment Submission assignment.

---
+++ Basic_C#_Programs/Method Class Assignment Submission/Method Class Assignment Submission/NumberOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment_Submission
+{
+    class NumberOperation
+    {
+        //Create a class, this class will have the method named Operation() and this method will return
+        //nothing and be a public method that can be accessed anywhere in the program.
+        public void Operation(int num1, int num2)
+        {
+            //Do the operation on the 1st number.
+            int result = num1 ^ 6;
+            //Display the second number onto the screen.
+            Console.WriteLine("You passed in {0} as the second input integer.", num2);
+        }
+    }
+}
---
+++ Basic_C#_Programs/Method Class Assignment Submission/Method Class Assignment Submission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the object
+            NumberOperation object1 = new NumberOperation();
+            //Call the Operation() method inside the object.
+            object1.Operation(12, 23);
+            //Call the Operation() method inside the object, this time with parameter names.
+            object1.Operation(num1: 12, num2: 22223);
+            Console.ReadLine();
+        }
+    }
+}
