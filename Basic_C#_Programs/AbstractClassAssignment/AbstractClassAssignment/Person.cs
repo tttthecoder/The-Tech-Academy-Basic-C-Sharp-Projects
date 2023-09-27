6a578665fe5413ee895902e06c35f50c750abd21
tttthecoder <trungtintran1905@gmail.com>
2023-09-27 17:08:01 -0600

    Added the folder for the abstract class assignment.

---
+++ Basic_C#_Programs/AbstractClassAssignment/AbstractClassAssignment/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    //keyword abstract make the Person class an abstract class which cannot be instantiated.
+    abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        //abstract keyword make the method SayName() an abstract method which cannot have an implementation.
+        public abstract void SayName();
+    }
+
+
+
+    //Employee class inherits from Person class
+    class Employee : Person
+    {
+        //Actual implementation of the SayName() abstract method. This sastisfies the contract with the abstract parent class.  
+        public override void SayName()
+        {
+            Console.WriteLine("My name: {0} {1}", firstName, lastName);
+        }
+
+    }
+}
+
+
+
+
---
+++ Basic_C#_Programs/AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an Emnployee, which morph into the Person class.
+            Person employee = new Employee() { firstName = "Tin", lastName = "Tran" };
+            //Call the method on the Employee object.
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
