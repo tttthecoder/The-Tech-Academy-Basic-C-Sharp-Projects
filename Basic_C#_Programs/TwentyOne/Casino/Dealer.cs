0f292e03352806f1f02d82a3f4d150ca291b9d02
tttthecoder <trungtintran1905@gmail.com>
2023-10-03 18:18:15 -0600

    Created the Casino library so that the TwentyOne game could make use of

---
+++ Basic_C#_Programs/TwentyOne/Casino/Dealer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Casino
+{
+    public class Dealer
+    {
+        public string Name { get; set; }
+        public Deck Deck { get; set; }
+        public int Balance { get; set; }
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First());
+            string card = Deck.Cards.First().ToString() + "\n";
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Admin\Desktop\log.txt", true))
+            {
+                DateTime dateTime = new DateTime();
+                file.WriteLine(card+ "-log created at "+ dateTime.ToString());
+            }
+            Console.WriteLine(card);
+            Deck.Cards.RemoveAt(0);
+        }
+    }
+}
---
+++ Basic_C#_Programs/TwentyOne/Casino/PLayer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public class Player
+    {
+        public Player(string name, int beginningBalance)
+        {
+            this.Hand = new List<Card>();
+            this.Name = name;
+            this.Balance = beginningBalance;
+        }
+
+        private List<Card> _hand = new List<Card>();
+        public List<Card> Hand { get { return _hand; } set { _hand = value; } }
+        public int Balance { get; set; }
+        public string Name { get; set; }
+        public bool isActivePlaying { get; set; }
+        public bool Stay { get; set; }
+        public Guid Id { get; set; }
+
+
+        public bool Bet(int amount)
+        {
+            if (Balance - amount < 0)
+            {
+                Console.WriteLine("You do not have enough to place a bet that size.");
+                return false;
+            }
+            else
+            {
+                Balance -= amount;
+                return true;
+            }
+        }
+
+        public static Game operator +(Game game, Player player)
+        {
+            game.Players.Add(player);
+            return game;
+        }
+        public static Game operator -(Game game, Player player)
+        {
+            game.Players.Remove(player);
+            return game;
+        }
+
+    }
+}
