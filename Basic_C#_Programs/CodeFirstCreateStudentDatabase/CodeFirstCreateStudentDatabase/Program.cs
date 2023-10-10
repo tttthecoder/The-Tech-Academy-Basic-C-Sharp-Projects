bda70cb3e4025dedb33f6d50607ab2186ee23a75
tttthecoder <trungtintran1905@gmail.com>
2023-10-10 13:34:31 -0600

    Added the folder CodeFirstCreateStudentDatabase.

---
+++ Basic_C#_Programs/CodeFirstCreateStudentDatabase/CodeFirstCreateStudentDatabase/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+namespace CodeFirstCreateStudentDatabase
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                //Create and save a new Student
+                Console.WriteLine("Enter a first name for a new Student: ");
+                var name = Console.ReadLine();
+
+                var student = new Student { FirstName = name };
+                db.Students.Add(student);
+                db.SaveChanges();
+
+
+                //Display all the Student from the database
+                var studentList = from s in db.Students
+                                  orderby s.FirstName
+                                  select s;
+
+                Console.WriteLine("All Students in the database");
+                foreach (var s in studentList)
+                {
+                    Console.WriteLine(s.FirstName);
+                }
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal GPA { get; set; }
+        public string EmailAddress { get; set; }
+    }
+
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
